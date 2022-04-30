a6bb69bf277f408b83c6d17a12af6652501bdcd9
RajGupta011 <rajgupta.skg9301@gmail.com>
2022-04-30 21:09:11 +0530

    Initial commit

--- JAVASCRIPT files/tablalogic.js
+++ JAVASCRIPT files/tablalogic.js
@@ -1,31 +1,143 @@
 
 
 window.addEventListener('keydown', (e) => {
-    
+
     let audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-    if(!audio) return;
-    audio.currentTime =0;
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
 })
 
 window.addEventListener('keydown', (ev) => {
-    
+
     let changeColor = document.querySelector(`button[data-key = '${ev.keyCode}']`)
-    
-    // setTimeout(() => changeColor.)
 
-    setTimeout(() => changeColor.style.backgroundColor=" rgb(136, 116, 0)", 300)
+   
 
-    setTimeout(() => changeColor.style.backgroundColor="white", 400)
+    setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+
+    setTimeout(() => changeColor.style.backgroundColor = "white", 400)
     
     
 })
 
-const jhaptaal = () => {
-    for(let i = 1; i<=100;i++){
-        let audio = document.querySelector(`audio[data-key='${6}']`)
-        audio.play();
+// const jhaptaal = () => {
+    
+// //    setInterval(() => {
+//     setTimeout(function beatDhi() {
+//         let audio = document.querySelector(`audio[data-key='68']`)
+//         audio.play();
+//         let changeColor = document.querySelector(`button[data-key = '68']`)
+//         setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
 
-    }
-}
+//         setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//         setTimeout(function beatNa() {
+//             let audio = document.querySelector(`audio[data-key='79']`)
+//             audio.play();
 
+//             let changeColor = document.querySelector(`button[data-key = '79']`)
+
+//             setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+
+//             setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//             setTimeout(function beatDhi() {
+//                 let audio = document.querySelector(`audio[data-key='68']`)
+//                 audio.play();
+//                 let changeColor = document.querySelector(`button[data-key = '68']`)
+
+               
+
+//                 setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+
+//                 setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//                 setTimeout(function beatDhi() {
+//                     let audio = document.querySelector(`audio[data-key='68']`)
+//                     audio.play();
+//                     let changeColor = document.querySelector(`button[data-key = '68']`)
+            
+                   
+            
+//                     setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+            
+//                     setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//                     setTimeout(function beatNa() {
+//                         let audio = document.querySelector(`audio[data-key='79']`)
+//                         audio.play();
+//                         let changeColor = document.querySelector(`button[data-key = '79']`)
+                
+                       
+                
+//                         setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                
+//                         setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//                         setTimeout(function beat1Tiri() {
+//                             let audio = document.querySelector(`audio[data-key='75']`)
+//                             audio.play();
+//                             let changeColor = document.querySelector(`button[data-key = '75']`)
+                    
+                           
+                    
+//                             setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                    
+//                             setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+                            
+//                             setTimeout(function beatNa() {
+//                                 let audio = document.querySelector(`audio[data-key='79']`)
+//                                 audio.play();
+//                                 let changeColor = document.querySelector(`button[data-key = '79']`)
+                        
+                               
+                        
+//                                 setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                        
+//                                 setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//                                 setTimeout(function beatDhi() {
+//                                     let audio = document.querySelector(`audio[data-key='68']`)
+//                                     audio.play();
+//                                     let changeColor = document.querySelector(`button[data-key = '68']`)
+                            
+                                   
+                            
+//                                     setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                            
+//                                     setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+//                                     setTimeout(function beatDhi() {
+//                                         let audio = document.querySelector(`audio[data-key='68']`)
+//                                         audio.play();
+//                                         let changeColor = document.querySelector(`button[data-key = '68']`)
+                                
+                                       
+                                
+//                                         setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                                
+//                                         setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+                                        
+//                                         setTimeout(function beatNa() {
+//                                             let audio = document.querySelector(`audio[data-key='79']`)
+//                                             audio.play();
+//                                             let changeColor = document.querySelector(`button[data-key = '79']`)
+                                    
+                                           
+                                    
+//                                             setTimeout(() => changeColor.style.backgroundColor = " rgb(136, 116, 0)", 300)
+                                    
+//                                             setTimeout(() => changeColor.style.backgroundColor = "white", 400)
+                                            
+//                                         }, 500)
+//                                     }, 500)
+//                                 }, 500)
+//                             }, 500)
+//                         }, 500)
+//                     }, 500)
+//                 }, 500)
+//             }, 500)
+//         }, 500)
+//     }, 500)
+    
+// // }, 1000)    
+    
+// }
+
+
+
+// jhaptaal()
